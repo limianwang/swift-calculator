63efbfe26762351a14dd5367a971fa4e6e0ebb51
Limian Wang <limian.wang@gmail.com>
2015-09-21 23:19:01 -0700

    add operations to the calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,18 +12,70 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
 
-    var userIsInTheMiddleOfTypingNumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
+
+    // initializing array of doubles
+    var operandStack = Array<Double>()
+
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+
+        switch operation {
+        case "×": performOperation({ $0 * $1 })
+        case "÷": performOperation({ $1 / $0 })
+        case "+": performOperation({ $0 + $1 })
+        case "−": performOperation({ $1 - $0 })
+        case "√": performOperation({ sqrt($0) })
+        default: break
+        }
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    @nonobjc
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        print("digit = \(digit)")
 
-        if userIsInTheMiddleOfTypingNumber {
+        if userIsInTheMiddleOfTypingANumber {
             display.text = display.text! + digit
         } else {
             display.text = digit
-            userIsInTheMiddleOfTypingNumber = true
+            userIsInTheMiddleOfTypingANumber = true
         }
     }
+
+
 }
 
